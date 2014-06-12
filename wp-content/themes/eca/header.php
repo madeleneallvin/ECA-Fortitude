6c944ac8efb26e50f32750b7fa8dd4505aa4dcb0
Madelene Allvin <madeleneallvin@maddesmac.lan>
2014-06-12 21:45:55 +0200

    Ny version av temat för eca

--- wp-content/themes/eca/header.php
+++ wp-content/themes/eca/header.php
@@ -24,6 +24,8 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href='http://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
